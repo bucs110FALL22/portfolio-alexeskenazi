4623ae4f2dbcab0ac423d725324980f9d52f16ca
Alex Eskenazi <112673600+alexeskenazi@users.noreply.github.com>
2022-09-06 22:36:41 +0000

    Completed exercise 1

---
+++ ch01/exercises/simple.py
@@ -0,0 +1,27 @@
+x1 = (10 * 5)
+x2 = 10 ** 2
+x3 = 15 / 10
+x4 = 15 // 10
+x5 = -15 // 10
+x6 = 15 % 10
+x7 = 10 % 15
+x8 = 10 % 10
+x9 = 0 % 10
+x10 = 10 / 15
+print(x1)
+print(x2)
+print(x3)
+print(x4)
+print(x5)
+print(x6)
+print(x7)
+print(x8)
+print(x9)
+print(x10)#the socalled infinite number a couple numbers.
+print ("What is the exchange rate from Euros to Dollars")
+rate = int( input("Please input a value: ") )
+print ("How many Euros are you converting into Dollars")
+amount = int( input("Please input a value ") )
+total = amount/rate
+result = total-3
+print (result)
