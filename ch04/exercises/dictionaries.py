2d4b0aacd2018164328b96faa59ba6e2bf786145
Alex Eskenazi <112673600+alexeskenazi@users.noreply.github.com>
2022-09-29 03:49:43 +0000

    Dictionaries.py

--- ch04/exercises/dictionaries.py
+++ ch04/exercises/dictionaries.py
@@ -1,6 +1,6 @@
 article = ""
 "Pokémon GO is one of the most successful mobile games of all time, breaking records like fastest to earn $100 million and most-downloaded in its first month of release. To date, it has grossed almost $2 billion in revenue and been downloaded 800 million times. Although no longer the global phenomenon it was in 2016, the game remains incredibly popular."
-""""""
+"""""
 substitutions = {
     "Pokemon":"Monsters",
     "successful":"boring",
@@ -14,9 +14,8 @@ substitutions = {
     "release":"beta",
     "billion":"USD",
     "phenomenon":"game",
-    "popular": "dead"
-}
+    "popular": "dead" }
+
 for key, value in substitutions.items():
    article = article.replace(key, value) 
-
 print(article)
