adfd8a18f690814e60364acd447be0df3a90e5c7
Alex Eskenazi <112673600+alexeskenazi@users.noreply.github.com>
2022-10-04 02:12:10 +0000

    coinflip

---
+++ ch04/exercises/coinflip.py
@@ -0,0 +1,13 @@
+import turtle
+import time
+import random
+
+turtle.screensize(200, 200)
+turtle.shape("turtle")
+turtle.color('red')
+time.sleep(2)
+
+run = True
+
+
+turtle.exitonclick()
--- ch04/exercises/dictionaries.py
+++ ch04/exercises/dictionaries.py
@@ -1,6 +1,5 @@
-article = ""
-"Pokémon GO is one of the most successful mobile games of all time, breaking records like fastest to earn $100 million and most-downloaded in its first month of release. To date, it has grossed almost $2 billion in revenue and been downloaded 800 million times. Although no longer the global phenomenon it was in 2016, the game remains incredibly popular."
-"""""
+article = "Pokémon GO is one of the most successful mobile games of all time, breaking records like fastest to earn $100 million and most-downloaded in its first month of release. To date, it has grossed almost $2 billion in revenue and been downloaded 800 million times. Although no longer the global phenomenon it was in 2016, the game remains incredibly popular."
+
 substitutions = {
     "Pokemon":"Monsters",
     "successful":"boring",
@@ -17,5 +16,5 @@ substitutions = {
     "popular": "dead" }
 
 for key, value in substitutions.items():
-   article = article.replace(key, value) 
+   article = article.replace(key, value)
 print(article)
