d5c568523fa7fb8a2ef01f1dee9f5f302773c785
Alex Eskenazi <112673600+alexeskenazi@users.noreply.github.com>
2022-10-10 16:54:21 +0000

    coinflip.py

--- ch04/exercises/coinflip.py
+++ ch04/exercises/coinflip.py
@@ -1,13 +1,27 @@
 import turtle
 import time
 import random
-
-turtle.screensize(200, 200)
+screensize = 200
+turtle.screensize(150,150)
+print(turtle.screensize())
 turtle.shape("turtle")
 turtle.color('red')
-time.sleep(2)
-
 run = True
-
-
+while run:
+  coin=random.randrange(0,2)
+  if coin==1:
+    turtle.left (90)
+  else: 
+    turtle.right (90)
+  screensize = turtle.screensize()[0]
+  turtle.forward (50)
+  if turtle.position() [0] >= (screensize/2):
+    run = False 
+  if turtle.position()[0] <= (screensize/-2):
+    run = False
+  if turtle.position()[1]>(screensize/2):
+    run = False 
+  if turtle.position()[1]<(screensize/-2):
+    run = False
+  print(turtle.position())
 turtle.exitonclick()
